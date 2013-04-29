31e3bfe201911290a08fcbc40f5333d728d5b237
jjorgenson.rdg@gmail.com <jjorgenson.rdg@gmail.com@yourdomain.com>
2013-04-29 13:00:33 +0000

    base project added to src directory

---
+++ src/macsur-adapter/macsur-adapter.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-04-29T11:25:07
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = macsur-adapter
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mamainform.cpp
+
+HEADERS  += mamainform.h
+
+FORMS    += mamainform.ui
---
+++ src/macsur-adapter/mamainform.cpp
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   File:  mamainform.cpp created: 29/04/2013                                    *
+ *   Class info: MaMainForm                                               *
+ *   Copyright (C) 2013 by: Jason S. Jorgenson                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "mamainform.h"
+#include "ui_mamainform.h"
+
+MaMainForm::MaMainForm(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MaMainForm)
+{
+    ui->setupUi(this);
+}
+
+MaMainForm::~MaMainForm()
+{
+    delete ui;
+}
+
